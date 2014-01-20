5f8813867e83bdd9a4f2c81745016ad1b778caa9
Safareli <i.safareli@gmail.com>
2014-01-21 03:38:46 +0400

    init grunt stuf

---
+++ Gruntfile.js
@@ -0,0 +1,149 @@
+module.exports = function(grunt) {
+    var live = grunt.option('live') ? true : false;
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        config: grunt.file.readJSON('config.json'),
+        connect: {
+            server: {
+                options: {
+                    hostname: '<%= config.web.origin %>',
+                    livereload: live,
+                    base: '<%= config.build.base %>',
+                    port: '<%= config.web.port %>',
+                },
+            },
+            alive: {
+                options: {
+                    hostname: '<%= config.web.origin %>',
+                    port: '<%= config.web.port %>',
+                    base: '<%= config.build.base %>',
+                    keepalive: true
+                }
+            }
+        },
+        compass: {
+            build: {
+                options: {
+                    noLineComments: '<%= config.compass.noLineComments %>',
+                    sassDir: '<%= config.compass.sassDir %>',
+                    cssDir: '<%= config.compass.cssDir %>',
+                    outputStyle: '<%= config.compass.outputStyle %>',
+                    fontsDir: '<%= config.compass.fontsDir %>',
+                    javascriptsDir: '<%= config.compass.javascriptsDir %>',
+                    imagesDir: '<%= config.compass.imagesDir %>',
+                    httpPath: '<%= config.compass.httpPath %>'
+                }
+            }
+        },
+        slim: {
+            slim: {
+                options: { // Target options
+                    style: 'expanded'
+                },
+                files: {
+                    '<%= config.build.base %>index.html': '<%= config.source.base %>index.slim',
+                }
+            }
+        },
+        watch: {
+            fonts: {
+                files: ['<%= config.source.fonts %>**'],
+                tasks: ['copy:fonts']
+            },
+            fontsBuild: {
+                files: ['<%= config.build.fonts %>**'],
+                options: {
+                    livereload: live,
+                }
+            },
+            images: {
+                files: ['<%= config.source.images %>**'],
+                tasks: ['copy:images']
+            },
+            imagesBuild: {
+                files: ['<%= config.build.images %>**'],
+                options: {
+                    livereload: live,
+                }
+            },
+            scripts: {
+                files: ['<%= config.source.scripts %>**'],
+                tasks: ['copy:scripts']
+            },
+            scriptsBuild: {
+                files: ['<%= config.build.scripts %>**'],
+                options: {
+                    livereload: live,
+                }
+            },
+            sass: {
+                files: ['<%= config.source.sass %>**'],
+                tasks: ['compass:build']
+            },
+            css: {
+                files: ['<%= config.build.css %>**'],
+                options: {
+                    livereload: live,
+                }
+            },
+            slim: {
+                files: ['<%= config.source.base %>/*.slim'],
+                tasks: ['slim:build']
+            },
+            html: {
+                files: ['<%= config.source.base %>*.html'],
+                options: {
+                    livereload: live,
+                }
+            }
+        },
+        clean: {
+            build: {
+                src: '<%= config.build.base %> '
+            }
+        },
+        copy: {
+            scripts: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= config.source.scripts %>',
+                    src: '**',
+                    dest: '<%= config.build.scripts %>/'
+                }]
+            },
+            fonts: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= config.source.fonts %>',
+                    src: '**',
+                    dest: '<%= config.build.fonts %>/'
+                }]
+            },
+            images: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= config.source.images %>',
+                    src: '**',
+                    dest: '<%= config.build.images %>/'
+                }]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-slim');
+
+    grunt.registerTask('default ', [
+        'clean:build ',
+        'copy:build ',
+        'compass:build ',
+        'slim:build ',
+        'connect:server ',
+        'watch '
+    ]);
+    grunt.registerTask('server ', ['connect:alive ']);
+};
