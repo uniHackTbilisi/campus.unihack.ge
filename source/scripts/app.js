bb688e165f9cc5774394416677d915a552d7dd28
Safareli <i.safareli@gmail.com>
2014-01-21 10:06:12 +0400

    split sass into component files; make slider;registration;menu

--- source/scripts/app.js
+++ source/scripts/app.js
@@ -1,1 +1,12 @@
-console.log("hi i'm app");
+(function(document, window) {
+    "use strict";
+    var body = document.body;
+    var onChange = function() {
+        var hash = window.location.hash || '#/main';
+        body.className = body.className.replace(/page\-[a-zA-Z]+/g, '').trim().concat(hash.replace('#/', ' page-'));
+        console.log(body.className);
+    };
+    window.onhashchange = onChange;
+    onChange();
+    body.className += ' transition';
+})(document, window);
