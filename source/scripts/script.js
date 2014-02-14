646e76d734a3b6bc13ef88922b597fa51e3558b6
otarza <otar.zakalashvili@gmail.com>
2014-02-14 12:06:14 +0400

    down arrow added, smooth scrolling added, faq text edited

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -113,6 +113,21 @@ jQuery(document).ready(function ($) {
     }
 });
 
+jQuery(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
 
 (function (i, s, o, g, r, a, m) {
     i['GoogleAnalyticsObject'] = r;
